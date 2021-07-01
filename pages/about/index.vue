cc33b122beb2f49b11a526008bbb9161247b48a0
Alexsandr <alexnem@inbox.ru>
2021-07-01 17:17:20 +0300

    add base scheme for project

---
+++ pages/about/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about-page">
+    <p>
+      Lorem ipsum
+      Lorem ipsum
+    </p>
+    <p>
+      Lorem ipsum
+      Lorem ipsum
+    </p>
+    <p>
+      Lorem ipsum
+      Lorem ipsum
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+
+</style>
