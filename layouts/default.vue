2eb85e48510aeda429d351100c62eb19792fcf20
Alexsandr <alexnem@inbox.ru>
2021-07-01 20:33:16 +0300

    add nav in header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
+    <the-header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <the-sidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  },
+}
+</script>
 
 <style>
 html {
@@ -31,32 +53,4 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
